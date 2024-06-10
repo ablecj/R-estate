a2bb757d3b7e5dcca5f6a7563145bf74b8c9d99f
able c j <gracyebin7@gamil.com>
2024-06-10 23:20:56 +0530

    real-estate project started

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/About';
+
+const App = () => {
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
