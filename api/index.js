4bace8dcf8356125e8ccbeb14faa0dc1fae381d3
able c j <gracyebin7@gamil.com>
2024-06-11 15:20:19 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGOURL).then(()=>{
+    console.log("monogodb connected to the dtatabase!")
+}).catch(()=>{
+    console.log("error occured!")
+})
 
 app.listen(3000, () => {
     console.log('server is running on port 3000!');
