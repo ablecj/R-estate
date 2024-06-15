7669aae60633992756a53b90889533e2c1869aef
able c j <gracyebin7@gamil.com>
2024-06-15 23:27:47 +0530

    Error handler implemented & password hashing

--- api/Controller/authController.js
+++ api/Controller/authController.js
@@ -1,7 +1,8 @@
 import User from '../Models/userModel.js'
 import bcrypt from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async(req,res)=> {
+export const signup = async(req,res, next)=> {
     const {username, email, password} = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = new User({username, email, password: hashedPassword});
@@ -9,6 +10,6 @@ export const signup = async(req,res)=> {
         await newUser.save();
         res.status(201).json({message: 'user created successfuly!'})   
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -26,5 +26,14 @@ app.use('/api/user', userRouter);
 
 app.use('/api/auth', authRouter);
 
-
+// middleware for calling error 
+app.use((err,req,res,next)=> {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
 
