15dc422aad3f02a9bbe1ad4106c8a35b1bec2137
able c j <gracyebin7@gamil.com>
2024-12-24 15:19:50 +0530

    update listing route backendd functionalitiy added

--- api/Controller/listingCntroller.js
+++ api/Controller/listingCntroller.js
@@ -30,3 +30,29 @@ export const deleteListing = async(req,res,next)=> {
         next(error);
     }
 } 
+
+// functionality for update listing details 
+export const updateListing = async (req, res, next)=> {
+     // find the item from the db and delete the value
+     const listing = await Listing.findById(req.params.id);
+     // check the condition where the listing is there or not 
+     if(!listing) {
+         return next(errorHandler(404, "Listing not found"))
+     }
+ 
+     if(req.user.id !== listing.userRef) {
+         return next(errorHandler(401, "You can only delete your own listings"))
+     }
+     try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        );
+        res.status(200).json(updatedListing);
+     } catch (error) {
+        next(error);
+     }
+}
+
+
--- api/Routes/listingRoute.js
+++ api/Routes/listingRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing } from '../Controller/listingCntroller.js';
+import { createListing, deleteListing, updateListing } from '../Controller/listingCntroller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -8,7 +8,9 @@ const router = express.Router();
 
 router.post('/create',verifyToken,createListing);
 // route for deleting the listing data of the user 
-router.delete('/delete/:id', verifyToken, deleteListing)
+router.delete('/delete/:id', verifyToken, deleteListing);
+// route for updating the listing route 
+router.post('/update/:id', verifyToken, updateListing)
 
 
 
