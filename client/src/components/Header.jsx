f9d6619a9ebcff4593e806b624e3b2bc03d0f1db
able c j <gracyebin7@gamil.com>
2025-01-15 23:26:33 +0530

    update header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,35 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {useSelector} from 'react-redux';
+import { useEffect, useState } from "react";
 
 export default function Header() {
   // showing the user is authenticated or not 
   const {currentUser} = useSelector(state => state.user)
 
-  // console.log(currentUser.avatar, "currentuser avatar")
+  // navigate using useNavigate 
+  const navigate = useNavigate();
+
+  // state for saving the searchterm 
+  const [searchTerm, setSearchTerm] = useState('');
+  // functionality for submiting the form functiuonality
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`)
+  }
+
+  // useEffect to allow the search term inside the query into the inout field 
+  useEffect(()=> {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+
+  },[location.search])
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -17,14 +40,17 @@ export default function Header() {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e)=> setSearchTerm(e.target.value)}
           />
-
-          <FaSearch className="text-slate-600" />
+          <button>
+          <FaSearch className="text-slate-600" />  
+          </button>
         </form>
 
         <ul className="flex gap-4">
