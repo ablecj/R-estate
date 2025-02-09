ef7b2d9b110f32798e961439cb495f2aa87d5999
able c j <gracyebin7@gamil.com>
2025-02-09 18:30:24 +0530

    development version 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
@@ -11,5 +12,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(),
+    // tailwindcss(),
+  ],
 })
